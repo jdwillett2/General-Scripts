cc0cc6a01904600ea6d583b7338f2cfd7f51f737
jdwillett2 <56878729+jdwillett2@users.noreply.github.com>
2020-02-18 16:15:44 -0500

    Add Cisco Amp folder

--- Install-Cisco-Amp-Multiple.ps1
+++
@@ -1,5 +0,0 @@
-Param([parameter(Mandatory = $true, ValueFromPipeline = $true)]$server)
-
-Process {
-	start-process "$pshome\powershell.exe" -argumentlist "-command .\Install-Cisco-Amp.ps1 $server"
-}
